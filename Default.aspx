aac9c25e9a3b395e5f08b40d08f62d33c560c70b
Manjunath Gururaja <manju.6685@gmail.com>
2025-06-30 22:47:21 -0700

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="manjuvs.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Exception, High CPU, and High Memory Example</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h1>App Service Performance Scenarios</h1>
+            <div class="button-group">
+                <asp:Button ID="Button1" runat="server" Text="Cause Crash" OnClick="Button1_Click" CssClass="btn btn-crash" />
+                <asp:Button ID="Button2" runat="server" Text="Cause High CPU" OnClick="Button2_Click" CssClass="btn btn-cpu" />
+                <asp:Button ID="Button3" runat="server" Text="Cause High Memory" OnClick="Button3_Click" CssClass="btn btn-memory" />               
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace manjuvs
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // This will cause a StackOverflowException
+            CauseStackOverflow();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // This will cause high CPU usage
+            CauseHighCPU();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            // This will cause high memory usage
+            CauseHighMemory();
+        }
+       
+        private void CauseStackOverflow()
+        {
+            CauseStackOverflow(); // Recursive call without a termination condition
+        }
+
+        private void CauseHighCPU()
+        {
+            while (true)
+            {
+                // Busy-wait loop to cause high CPU usage
+            }
+        }
+
+        private void CauseHighMemory()
+        {
+            List<byte[]> memoryHog = new List<byte[]>();
+            while (true)
+            {
+                // Allocate large chunks of memory in a loop
+                memoryHog.Add(new byte[1024 * 1024]); // Allocate 1 MB
+            }
+        }
+    }
+}
